e5741fefcc08bae7239fbad701baa1d235e927f9
PotassiumIodide <imamura.suuri@gmail.com>
2020-11-13 22:18:11 +0900

    [Update] Increased test cases in test_checker.py

--- test/matroids/test_checker.py
+++ test/matroids/test_checker.py
@@ -13,56 +13,73 @@ from src.matroids.checker import (
 
 
 @pytest.mark.parametrize('maybe_matroid, expected', [
-    (( {1, 2, 3}, [set(), {1}, {2}, {3}, {1, 2}, {1, 3}]                    ),  True),
-    (( {1, 2, 3}, [set(), {1}, {2}, {3}, {1, 2}, {1, 3}, {2, 3}]            ),  True),
-    (( {1, 2, 3}, [set(), {1}, {2}, {3}, {1, 2}, {1, 3}, {2, 3}, {1, 2, 3}] ),  True),
-    (( {1, 2, 3}, [{1}, {2}, {3}]                                           ), False),
-    (( {1, 2, 3}, [set(), {1}, {2}, {3}, {1, 3}]                            ), False),
-    (( {1, 2, 3}, [set(), {1}, {1, 2}]                                      ), False),
-    (( {1, 2}   , [set(), {1}, {2}, {3}, {1, 2}, {1, 3}]                    ), False), 
-    (( {4, 5, 6}, [set(), {1}, {2}, {3}, {1, 2}, {1, 3}]                    ), False),
+    (( {1,2,3}, [set()] )                                      ,  True),
+    (( {1,2,3}, [set(),{1}] )                                  ,  True),
+    (( {1,2,3}, [set(),{1},{2}] )                              ,  True),
+    (( {1,2,3}, [set(),{1},{2},{3}] )                          ,  True),
+    (( {1,2,3}, [set(),{1},{2},{1,2}] )                        ,  True),
+    (( {1,2,3}, [set(),{1},{2},{3},{1,2},{1,3}] )              ,  True),
+    (( {1,2,3}, [set(),{1},{2},{3},{1,2},{1,3},{2,3}] )        ,  True),
+    (( {1,2,3}, [set(),{1},{2},{3},{1,2},{1,3},{2,3},{1,2,3}] ),  True),
+    (( {1,2,3}, [{1},{2},{3}] )                                , False),
+    (( {1,2,3}, [set(),{1},{2},{3},{1,3}] )                    , False),
+    (( {1,2,3}, [set(),{1},{1,2}] )                            , False),
+    (( {1,2}  , [set(),{1},{2},{3},{1,2},{1,3}] )              , False), 
+    (( {4,5,6}, [set(),{1},{2},{3},{1,2},{1,3}] )              , False),
 ])
 def test_satisfies_independent_axiom(maybe_matroid, expected):
     assert satisfies_independent_axiom(maybe_matroid) == expected
 
 
 @pytest.mark.parametrize('maybe_matroid, expected', [
-    (( {1, 2, 3}, [{2, 3}, {1, 2, 3}] )                       ,  True),
-    (( {1, 2, 3}, [{1, 2, 3}] )                               ,  True),
-    (( {1, 2, 3}, [] )                                        ,  True),
-    (( {1, 2, 3}, [set(), {1, 2}, {1, 3}, {2, 3}, {1, 2, 3}] ), False),
-    (( {1, 2, 3}, [{1, 2}, {2, 3}, {1, 2, 3}] )               , False),
-    (( {1, 2, 3}, [{2}, {3}, {1, 3}, {2, 3}, {1, 2, 3}] )     , False),
-    (( {1, 2}   , [{2, 3}, {1, 2, 3}] )                       , False), 
-    (( {4, 5, 6}, [{2, 3}, {1, 2, 3}] )                       , False),
+    (( {1,2,3}, [{1},{2},{3},{1,2},{1,3},{2,3},{1,2,3}] ),  True),
+    (( {1,2,3}, [{2},{3},{1,2},{1,3},{2,3},{1,2,3}] )    ,  True),
+    (( {1,2,3}, [{3},{1,2},{1,3},{2,3},{1,2,3}] )        ,  True),
+    (( {1,2,3}, [{1,2},{1,3},{2,3},{1,2,3}] )            ,  True),
+    (( {1,2,3}, [{3},{1,3},{2,3},{1,2,3}] )              ,  True),
+    (( {1,2,3}, [{2,3},{1,2,3}] )                        ,  True),
+    (( {1,2,3}, [{1,2,3}] )                              ,  True),
+    (( {1,2,3}, [] )                                     ,  True),
+    (( {1,2,3}, [set(),{1,2},{1,3},{2,3},{1,2,3}] )      , False),
+    (( {1,2,3}, [{1,2},{2,3},{1,2,3}] )                  , False),
+    (( {1,2,3}, [{2},{3},{1,3},{2,3},{1,2,3}] )          , False),
+    (( {1,2}  , [{2,3},{1,2,3}] )                        , False), 
+    (( {4,5,6}, [{2,3},{1,2,3}] )                        , False),
 ])
 def test_satisfies_dependent_axiom(maybe_matroid, expected):
     assert satisfies_dependent_axiom(maybe_matroid) == expected
 
 
 @pytest.mark.parametrize('maybe_matroid, expected', [
-    (( {1, 2, 3}, [set()]          ),  True),
-    (( {1, 2, 3}, [{1}, {2}, {3}]  ),  True),
-    (( {1, 2, 3}, [{1, 2}, {1, 3}] ),  True),
-    (( {1, 2, 3}, [{1, 2, 3}]      ),  True),
-    (( {1, 2, 3}, [{1}, {2, 3}]    ), False),
-    (( {1, 2, 3}, [{1, 4}, {2, 3}] ), False),
-    (( {4, 5, 6}, [{1, 2}, {1, 3}] ), False),
-    (( {1, 2, 3}, []               ), False)
+    (( {1,2,3}, [set()] )            ,  True),
+    (( {1,2,3}, [{1}] )              ,  True),
+    (( {1,2,3}, [{1},{2}] )          ,  True),
+    (( {1,2,3}, [{1},{2},{3}] )      ,  True),
+    (( {1,2,3}, [{1,2}] )            ,  True),
+    (( {1,2,3}, [{1,2},{1,3}] )      ,  True),
+    (( {1,2,3}, [{1,2},{1,3},{2,3}] ),  True),
+    (( {1,2,3}, [{1,2,3}] )          ,  True),
+    (( {1,2,3}, [{1},{2,3}] )        , False),
+    (( {1,2,3}, [{1,4},{2,3}] )      , False),
+    (( {4,5,6}, [{1,2},{1,3}] )      , False),
+    (( {1,2,3}, [] )                 , False)
 ])
 def test_satisfies_bases_axiom(maybe_matroid, expected):
     assert satisfies_bases_axiom(maybe_matroid) == expected
 
 
 @pytest.mark.parametrize('maybe_matroid, expected', [
-    (( {1, 2, 3}, [{3}]            ),  True),
-    (( {1, 2, 3}, [{1}, {2}, {3}]  ),  True),
-    (( {1, 2, 3}, [{1, 2}, {3}]    ),  True),
-    (( {1, 2, 3}, [{1, 2, 3}]      ),  True),
-    (( {1, 2, 3}, []               ),  True),
-    (( {1, 2, 3}, [{1, 3}, {2, 3}] ), False),
-    (( {1, 2, 3}, [{1}, {1, 2}]    ), False),
-    (( {4, 5, 6}, [{1, 2}, {3}]    ), False),
+    (( {1,2,3}, [{1},{2},{3}] )      ,  True),
+    (( {1,2,3}, [{2},{3}] )          ,  True),
+    (( {1,2,3}, [{3},{1,2}] )        ,  True),
+    (( {1,2,3}, [{1,2},{1,3},{2,3}] ),  True),
+    (( {1,2,3}, [{3}] )              ,  True),
+    (( {1,2,3}, [{2,3}] )            ,  True),
+    (( {1,2,3}, [{1,2,3}] )          ,  True),
+    (( {1,2,3}, [] )                 ,  True),
+    (( {1,2,3}, [{1,3},{2,3}] )      , False),
+    (( {1,2,3}, [{1},{1,2}] )        , False),
+    (( {4,5,6}, [{1,2},{3}] )        , False),
 ])
 def test_satisfies_circuits_axiom(maybe_matroid, expected):
     assert satisfies_circuits_axiom(maybe_matroid) == expected
@@ -79,16 +96,19 @@ def f(x: set) -> int:
 
 
 @pytest.mark.parametrize('maybe_matroid, expected', [
-    (( {1, 2, 3}, len                                                     ),  True),
-    (( {1, 2, 3}, lambda x: 2 if x == {1, 2, 3} else len(x)               ),  True),
-    (( {1, 2, 3}, lambda x: 1 if x else 0                                 ),  True),
-    (( {1, 2, 3}, lambda x: len(x)-1 if x in [{1,2,3}, {2,3}] else len(x) ),  True),
-    (( set()    , lambda x: 0                                             ),  True),
-    (( {1, 2, 3}, lambda x: len(x) if x else 1                  ), False),
-    (( {1, 2, 3}, lambda x: 0 if x == {1, 2, 3} or not x else 1 ), False),
-    (( {1, 2, 3}, lambda x: 2 * len(x)                          ), False),
-    (( {1, 2, 3}, f                                             ), False),
-    (( set()    , lambda x: 1                                   ), False),
+    (( {1,2,3}, lambda X: 0 )                                   ,  True),
+    (( {1,2,3}, lambda X: 1 if 1 in X else 0 )                  ,  True),
+    (( {1,2,3}, lambda X: 0 if X <= {3} else 1 )                ,  True),
+    (( {1,2,3}, lambda X: 1 if X else 0 )                       ,  True),
+    (( {1,2,3}, lambda X: len(X) - 1 if 3 in X else len(X) )    ,  True),
+    (( {1,2,3}, lambda X: len(X) - 1 if {2,3} <= X else len(X) ),  True),
+    (( {1,2,3}, lambda X: 2 if X == {1,2,3} else len(X) )       ,  True),
+    (( {1,2,3}, len )                                           ,  True),
+    (( {1,2,3}, lambda x: len(x) if x else 1 )                  , False),
+    (( {1,2,3}, lambda x: 0 if x == {1, 2, 3} or not x else 1 ) , False),
+    (( {1,2,3}, lambda x: 2 * len(x) )                          , False),
+    (( {1,2,3}, f )                                             , False),
+    (( set()  , lambda x: 1 )                                   , False),
 ])
 def test_satisfies_rank_function_axiom(maybe_matroid, expected):
     assert satisfies_rank_function_axiom(maybe_matroid) == expected
@@ -116,15 +136,19 @@ def ncl2(x: set[int]) -> set[int]:
 
 
 @pytest.mark.parametrize('maybe_matroid, expected', [
-    (( {1, 2, 3}, lambda x: x                               ),  True),
-    (( {1, 2, 3}, lambda x: x if len(x) <= 1 else {1, 2, 3} ),  True),
-    (( {1, 2, 3}, lambda x: {1, 2, 3} if x else x           ),  True),
-    (( set()    , lambda x: x                               ),  True),
-    (( {1, 2, 3}, lambda x: x if x else {1, 2, 3}           ), False),
-    (( {1, 2, 3}, lambda x: {1}                             ), False),
-    (( {1, 2, 3}, lambda x: {1, 2, 3} - x                   ), False),
-    (( {1, 2, 3}, ncl1                                      ), False),
-    (( {1, 2, 3}, ncl2                                      ), False),
+    (( {1,2,3}, lambda X: {1,2,3} )                      ,  True),
+    (( {1,2,3}, lambda X: {1,2,3} if 1 in X else {2,3} ) ,  True),
+    (( {1,2,3}, lambda X: {3} if X <= {3} else {1,2,3} ) ,  True),
+    (( {1,2,3}, lambda X: {1,2,3} if X else X )          ,  True),
+    (( {1,2,3}, lambda X: X | {3} )                      ,  True),
+    (( {1,2,3}, lambda X: X if X <= {1} else X | {2,3} ) ,  True),
+    (( {1,2,3}, lambda X: X if len(X) <= 1 else {1,2,3} ),  True),
+    (( {1,2,3}, lambda X: X )                            ,  True),
+    (( {1,2,3}, lambda x: x if x else {1, 2, 3} )        , False),
+    (( {1,2,3}, lambda x: {1} )                          , False),
+    (( {1,2,3}, lambda x: {1, 2, 3} - x )                , False),
+    (( {1,2,3}, ncl1 )                                   , False),
+    (( {1,2,3}, ncl2 )                                   , False),
 ])
 def test_satisfies_closure_function_axiom(maybe_matroid, expected):
     assert satisfies_closure_axiom(maybe_matroid) == expected
