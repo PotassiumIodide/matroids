8798cb664d58b8e3fe8d1dd2d3114a618b26da58
PotassiumIodide <imamura.suuri@gmail.com>
2020-11-14 23:12:08 +0900

    [Add] Enabled to construct a closure function from a matroid defined by spanning sets.

--- src/matroids/construct.py
+++ src/matroids/construct.py
@@ -716,6 +716,19 @@ def closure_function_from_hyperplanes_matroid(matroid: tuple[set[T], list[set[T]
     return closure_function_from_rank_matroid((E, rank_function_from_hyperplanes_matroid(matroid)))
 
 
+def closure_function_from_spanning_sets_matroid(matroid: tuple[set[T], list[set[T]]]) -> Callable[[set[T]], set[T]]:
+    """Construct a closure function from a matroid defined by spanning sets.
+
+    Args:
+        matroid (tuple[set[T], list[set[T]]]): A matroid defined by spanning sets.
+
+    Returns:
+        Callable[[set[T]], set[T]]: The closure function of a given matroid.
+    """
+    E, _ = matroid
+    return closure_function_from_rank_matroid((E, rank_function_from_spanning_sets_matroid(matroid)))
+
+
 def flats_from_rank_matroid(matroid: tuple[set[T], Callable[[set[T]], int]]) -> list[set[T]]:
     """Construct flats from a matroid defined by a rank function.
 
--- src/matroids/matroid.py
+++ src/matroids/matroid.py
@@ -165,10 +165,10 @@ class Matroid(Generic[T]):
             return construct.closure_function_from_open_sets_matroid((self.__first, self.__second))
         if self.__base_axiom is MatroidAxiom.HYPERPLANES:
             return construct.closure_function_from_hyperplanes_matroid((self.__first, self.__second))
+        if self.__base_axiom is MatroidAxiom.SPANNING_SETS:
+            return construct.closure_function_from_spanning_sets_matroid((self.__first, self.__second))
         
-        # TODO: Implement the other constructions from other axioms
-        else:
-            raise NotImplementedError(f"Implementation for {self.__base_axiom} has not defined yet!")
+        raise NotImplementedError(f"Implementation for {self.__base_axiom} has not defined yet!")
     
     @property
     def flats(self) -> list[set[T]]:
