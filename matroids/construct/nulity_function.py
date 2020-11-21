5f6e8054dc55885f09807ab96f1e3bcaf246605f
PotassiumIodide <imamura.suuri@gmail.com>
2020-11-21 13:46:15 +0900

    [Add] Add functions constructing a nulity function from a matroid

--- matroids/construct/nulity_function.py
+++ matroids/construct/nulity_function.py
@@ -1,9 +1,8 @@
 from typing    import Callable, TypeVar
 
-from matroids.construct import (
-    independent_sets,
-    rank_function
-)
+from matroids.core.set_operator import powset
+
+from matroids.construct import independent_sets
 
 T = TypeVar('T')
 
@@ -47,6 +46,19 @@ def from_bases(matroid: tuple[set[T], list[set[T]]]) -> Callable[[set[T]], int]:
     return from_independent_matroid((E, independent_sets.from_bases_matroid(matroid)))
 
 
+def from_circuits(matroid: tuple[set[T], list[set[T]]]) -> Callable[[set[T]], int]:
+    """Construct a nulity function from a matroid defined by circuits.
+
+    Args:
+        matroid (tuple[set[T], list[set[T]]]): A matroid defined by circuits.
+
+    Returns:
+        Callable[[set[T]], int]: The nulity function of a given matroid.
+    """
+    E, _ = matroid
+    return from_independent_matroid((E, independent_sets.from_circuits_matroid(matroid)))
+
+
 def from_rank_matroid(matroid: tuple[set[T], Callable[[set[T]], int]]) -> Callable[[set[T]], int]:
     """Construct a nulity function from a matroid defined by a rank function.
 
@@ -58,3 +70,69 @@ def from_rank_matroid(matroid: tuple[set[T], Callable[[set[T]], int]]) -> Callab
     """
     E, r = matroid
     return lambda X: len(X) - r(X)
+
+
+def from_closure_matroid(matroid: tuple[set[T], Callable[[set[T]], set[T]]]) -> Callable[[set[T]], int]:
+    """Construct a nulity function from a matroid defined by a closure function.
+
+    Args:
+        matroid (tuple[set[T], Callable[[set[T]], set[T]]]): A matroid defined by a closure function
+
+    Returns:
+        Callable[[set[T]], int]: The nulity function of a given matroid.
+    """
+    E, cl = matroid
+    # n(X) = |X| - min{ |I| : X ⊆ cl(I) }.
+    return lambda X: len(X) - min(len(I) for I in powset(E) if X <= cl(I))
+
+
+def from_flats_matroid(matroid: tuple[set[T], list[set[T]]]) -> Callable[[set[T]], int]:
+    """Construct a nulity function from a matroid defined by flats.
+
+    Args:
+        matroid (tuple[set[T], list[set[T]]]): A matroid defined by flats.
+
+    Returns:
+        Callable[[set[T]], int]: The nulity function of a given matroid.
+    """
+    E, _ = matroid
+    return from_independent_matroid((E, independent_sets.from_flats_matroid(matroid)))
+
+
+def from_open_matroid(matroid: tuple[set[T], list[set[T]]]) -> Callable[[set[T]], int]:
+    """Construct a nulity function from a matroid defined by open sets.
+
+    Args:
+        matroid (tuple[set[T], list[set[T]]]): A matroid defined by open sets.
+
+    Returns:
+        Callable[[set[T]], int]: The nulity function of a matroid.
+    """
+    E, _ = matroid
+    return from_independent_matroid((E, independent_sets.from_open_matroid(matroid)))
+
+
+def from_hyperplanes_matroid(matroid: tuple[set[T], list[set[T]]]) -> Callable[[set[T]], int]:
+    """Construct a nulity function from a matroid defined by hyperplanes
+
+    Args:
+        matroid (tuple[set[T], list[set[T]]]): A matroid defined by hyperplanes
+
+    Returns:
+        Callable[[set[T]], int]: The nulity function of a given matroid.
+    """
+    E, _ = matroid
+    return from_independent_matroid((E, independent_sets.from_hyperplanes_matroid(matroid)))
+
+
+def from_spanning_matroid(matroid: tuple[set[T], list[set[T]]]) -> Callable[[set[T]], int]:
+    """Construct a nulity function from a matroid defined by spanning sets.
+
+    Args:
+        matroid (tuple[set[T], list[set[T]]]): A matroid defined by spanning sets.
+
+    Returns:
+        Callable[[set[T]], int]: The nulity function of a given matroid.
+    """
+    E, _ = matroid
+    return from_independent_matroid((E, independent_sets.from_spanning_matroid(matroid)))
