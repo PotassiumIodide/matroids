398beaed691ff935334d47451951d0cbf9328287
PotassiumIodide <imamura.suuri@gmail.com>
2020-11-25 03:00:51 +0900

    [Update] Release v.3.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="matroids",
-    version="2.2.1",
+    version="3.0.0",
     author="Potassium Iodide",
     author_email="potassium.iodide28@gmail.com",
     description="Matroid",
